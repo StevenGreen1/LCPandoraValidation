a5e49f826b1fdcd4609c0e76c293d087fa1f9012
Steven Green <sg1sg2sg3@hotmail.co.uk>
2016-10-13 14:43:56 +0100

    Corrected typo in AnalysePerformance.py.

--- AnalysePerformance/AnalysePerformance.py
+++ AnalysePerformance/AnalysePerformance.py
@@ -16,24 +16,24 @@ for setting in ['Local', 'Release']:
             results += setting + ' ' + pandoraSettings + ' ' + str(energy) + 'GeV\n'
             results += '-----------------------------------------------------------------------------------------------------------------------------------\n'
 
-        inputRootFileFormat = 'Validating_' + setting + '_PandoraSettings' + pandoraSettings + '_Z_uds_' + str(energy) + '_GeV_Job_Number_*.root'
+            inputRootFileFormat = 'Validating_' + setting + '_PandoraSettings' + pandoraSettings + '_Z_uds_' + str(energy) + '_GeV_Job_Number_*.root'
 
-        if 'Local' in setting:
-            argsString = localBinary + ' ' + os.path.join(inputRootFolder, inputRootFileFormat)
-        else:
-            argsString = releasedBinary + ' ' + os.path.join(inputRootFolder, inputRootFileFormat)
+            if 'Local' in setting:
+                argsString = localBinary + ' ' + os.path.join(inputRootFolder, inputRootFileFormat)
+            else:
+                argsString = releasedBinary + ' ' + os.path.join(inputRootFolder, inputRootFileFormat)
 
-        args = argsString.split()
-        popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-        popen.wait()
-        output = popen.stdout.read()
+            args = argsString.split()
+            popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+            popen.wait()
+            output = popen.stdout.read()
 
-        resultsLine = ''
-        for line in output.splitlines():
-            if 'fPFA_L7A' in line:
-                resultsLine = line
+            resultsLine = ''
+            for line in output.splitlines():
+                if 'fPFA_L7A' in line:
+                    resultsLine = line
 
-        results += resultsLine + '\n'
+            results += resultsLine + '\n'
 
     resultsFileName = setting + '_JetEnergyResolutions.txt'
     textFile = open(resultsFileName, 'w')
